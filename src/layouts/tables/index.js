f453259b097f735b3ed397647f486b28ddb51a54
verreke <verreke@naver.com>
2023-12-24 14:43:49 +0900

    HB10, HB13 의 거래내역이 표기되도록 수정

--- src/layouts/tables/index.js
+++ src/layouts/tables/index.js
@@ -49,8 +49,32 @@ function Tables() {
   console.log(rows);
 
   const [hb10Info, setHb10Info] = useState(rows);
+  const [hb13Info, setHb13Info] = useState(rows);
   const urlHb10Stock = `http://3.35.228.162:3000/hb10stock`;
+  const urlHb13Stock = `http://3.35.228.162:3000/hb13stock`;
   const [startDate, setStartDate] = useState(new Date());
+
+  const handleDateChange = async (date) => {
+    setStartDate(date);
+    const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+    const formattedDate = date
+      .toLocaleDateString("ko-KR", options)
+      .replace(/\./g, "")
+      .replace(/ /g, "/");
+    console.log("handleDateChange");
+    console.log(formattedDate);
+
+    const params = {
+      date: formattedDate, // YYYY/MM/DD 포맷
+    };
+
+    const resHb10stock = await axios.get(urlHb10Stock, { params });
+    setHb10Info(resHb10stock.data.filter((stock) => stock.buy_hb10_1_count > 0));
+
+    const resHb13stock = await axios.get(urlHb13Stock, { params });
+    console.log(resHb13stock.data);
+    setHb13Info(resHb13stock.data.filter((stock) => stock.buy_hb13_1_count > 0));
+  };
 
   useEffect(() => {
     const options = { year: "numeric", month: "2-digit", day: "2-digit" };
@@ -66,13 +90,13 @@ function Tables() {
 
     async function getchStcokInfo() {
       const resHb10stock = await axios.get(urlHb10Stock, { params });
-      console.log(resHb10stock.data);
-
       const rowHb10stock = resHb10stock.data.filter((stock) => stock.buy_hb10_1_count > 0);
 
-      console.log(rowHb10stock);
+      const resHb13stock = await axios.get(urlHb13Stock, { params });
+      const rowHb13stock = resHb13stock.data.filter((stock) => stock.buy_hb13_1_count > 0);
 
       setHb10Info(rowHb10stock);
+      setHb13Info(rowHb13stock);
     }
     getchStcokInfo();
   }, []);
@@ -80,6 +104,7 @@ function Tables() {
   return (
     <DashboardLayout>
       <DashboardNavbar />
+      <DatePicker selected={startDate} onChange={handleDateChange} />
       <MDBox pt={6} pb={3}>
         <Grid container spacing={6}>
           <Grid item xs={12}>
@@ -98,22 +123,35 @@ function Tables() {
                   HB10 거래 내역
                 </MDTypography>
               </MDBox>
-              <MDBox pt={3}>
-                <TableContainer>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>종목</TableCell>
-                        <TableCell>매수1</TableCell>
-                        <TableCell>매수2</TableCell>
-                        <TableCell>매수3</TableCell>
-                        <TableCell>매도</TableCell>
-                      </TableRow>
-                    </TableHead>
+              <MDBox pt={3} style={{ overflowX: "auto" }}>
+                <TableContainer style={{ fontSize: "5px", minWidth: "600px" }}>
+                  <Table style={{ width: "100%" }}>
                     <TableBody>
+                      <TableRow>
+                        <TableCell>
+                          <strong>종목</strong>
+                        </TableCell>
+                        <TableCell>
+                          <strong>매수1</strong>
+                        </TableCell>
+                        <TableCell>
+                          <strong>매수2</strong>
+                        </TableCell>
+                        <TableCell>
+                          <strong>매수3</strong>
+                        </TableCell>
+                        <TableCell>
+                          <strong>매도</strong>
+                        </TableCell>
+                        <TableCell>
+                          <strong>손익</strong>
+                        </TableCell>
+                      </TableRow>
                       {hb10Info.map((stock) => (
                         <TableRow key={stock.stock_code}>
-                          <TableCell>{stock.stock_name}</TableCell>
+                          <TableCell component="th" scope="row">
+                            {stock.stock_name}
+                          </TableCell>
                           <TableCell>
                             {stock.buy_hb10_1_count > 0
                               ? `${stock.buy_hb10_1_price}\n(${stock.buy_hb10_1_count})`
@@ -130,8 +168,29 @@ function Tables() {
                               : `-`}
                           </TableCell>
                           <TableCell>
+                            {stock.sell_count > 0 ? (
+                              Number(stock.buy_hb10_1_price) * Number(stock.buy_hb10_1_count) +
+                                Number(stock.buy_hb10_2_price) * Number(stock.buy_hb10_2_count) +
+                                Number(stock.buy_hb10_3_price) * Number(stock.buy_hb10_3_count) <
+                              Number(stock.sell_price) * Number(stock.sell_count) ? (
+                                <strong style={{ color: "red" }}>
+                                  {`${stock.sell_price}\n(${stock.sell_count})`}
+                                </strong>
+                              ) : (
+                                <strong style={{ color: "blue" }}>
+                                  {`${stock.sell_price}\n(${stock.sell_count})`}
+                                </strong>
+                              )
+                            ) : (
+                              `-`
+                            )}
+                          </TableCell>
+                          <TableCell>
                             {stock.sell_count > 0
-                              ? `${stock.sell_price}\n(${stock.sell_count})`
+                              ? Number(stock.sell_price) * Number(stock.sell_count) -
+                                (Number(stock.buy_hb10_1_price) * Number(stock.buy_hb10_1_count) +
+                                  Number(stock.buy_hb10_2_price) * Number(stock.buy_hb10_2_count) +
+                                  Number(stock.buy_hb10_3_price) * Number(stock.buy_hb10_3_count))
                               : `-`}
                           </TableCell>
                         </TableRow>
@@ -139,14 +198,6 @@ function Tables() {
                     </TableBody>
                   </Table>
                 </TableContainer>
-
-                {/* <DataTable
-                  table={{ columns, hb10Info }}
-                  isSorted={false}
-                  entriesPerPage={false}
-                  showTotalEntries={false}
-                  noEndBorder
-                /> */}
               </MDBox>
             </Card>
           </Grid>
@@ -163,17 +214,84 @@ function Tables() {
                 coloredShadow="info"
               >
                 <MDTypography variant="h6" color="white">
-                  Projects Table
+                  HB13 거래내역
                 </MDTypography>
               </MDBox>
-              <MDBox pt={3}>
-                <DataTable
-                  table={{ columns: pColumns, rows: pRows }}
-                  isSorted={false}
-                  entriesPerPage={false}
-                  showTotalEntries={false}
-                  noEndBorder
-                />
+              <MDBox pt={3} style={{ overflowX: "auto" }}>
+                <TableContainer style={{ fontSize: "5px", minWidth: "600px" }}>
+                  <Table style={{ width: "100%" }}>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell>
+                          <strong>종목</strong>
+                        </TableCell>
+                        <TableCell>
+                          <strong>매수1</strong>
+                        </TableCell>
+                        <TableCell>
+                          <strong>매수2</strong>
+                        </TableCell>
+                        <TableCell>
+                          <strong>매수3</strong>
+                        </TableCell>
+                        <TableCell>
+                          <strong>매도</strong>
+                        </TableCell>
+                        <TableCell>
+                          <strong>손익</strong>
+                        </TableCell>
+                      </TableRow>
+                      {hb13Info.map((stock) => (
+                        <TableRow key={stock.stock_code}>
+                          <TableCell component="th" scope="row">
+                            {stock.stock_name}
+                          </TableCell>
+                          <TableCell>
+                            {stock.buy_hb13_1_count > 0
+                              ? `${stock.buy_hb13_1_price}\n(${stock.buy_hb13_1_count})`
+                              : `-`}
+                          </TableCell>
+                          <TableCell>
+                            {stock.buy_hb13_2_count > 0
+                              ? `${stock.buy_hb13_2_price}\n(${stock.buy_hb13_2_count})`
+                              : `-`}
+                          </TableCell>
+                          <TableCell>
+                            {stock.buy_hb13_3_count > 0
+                              ? `${stock.buy_hb13_3_price}\n(${stock.buy_hb13_3_count})`
+                              : `-`}
+                          </TableCell>
+                          <TableCell>
+                            {stock.sell_hb13_count > 0 ? (
+                              Number(stock.buy_hb13_1_price) * Number(stock.buy_hb13_1_count) +
+                                Number(stock.buy_hb13_2_price) * Number(stock.buy_hb13_2_count) +
+                                Number(stock.buy_hb13_3_price) * Number(stock.buy_hb13_3_count) <
+                              Number(stock.sell_hb13_price) * Number(stock.sell_hb13_count) ? (
+                                <strong style={{ color: "red" }}>
+                                  {`${stock.sell_hb13_price}\n(${stock.sell_hb13_count})`}
+                                </strong>
+                              ) : (
+                                <strong style={{ color: "blue" }}>
+                                  {`${stock.sell_hb13_price}\n(${stock.sell_hb13_count})`}
+                                </strong>
+                              )
+                            ) : (
+                              `-`
+                            )}
+                          </TableCell>
+                          <TableCell>
+                            {stock.sell_hb13_count > 0
+                              ? Number(stock.sell_hb13_price) * Number(stock.sell_hb13_count) -
+                                (Number(stock.buy_hb13_1_price) * Number(stock.buy_hb13_1_count) +
+                                  Number(stock.buy_hb13_2_price) * Number(stock.buy_hb13_2_count) +
+                                  Number(stock.buy_hb13_3_price) * Number(stock.buy_hb13_3_count))
+                              : `-`}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
               </MDBox>
             </Card>
           </Grid>
